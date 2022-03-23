de52fd8860f429d14710b319387ee7285ee3a4b7
skeeven1 <91453728+skeeven1@users.noreply.github.com>
2022-03-23 19:21:08 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -34,7 +34,20 @@ function RandomImg1() {
   for (let i = 1; i < 10; i++) {
     var randomNum = Math.floor(Math.random() * jojoImg.length);
     document.getElementById("jojo" + i.toString()).src = jojoImg[randomNum];
-} 
+    
+    var randomNum2 = Math.floor(Math.random() * animeImg.length);
+    document.getElementById("anime" + i.toString()).src = animeImg[randomNum2];
+    
+    var randomNum3 = Math.floor(Math.random() * animeImg.length);
+    document.getElementById("japan_series" + i.toString()).src = japan_series[randomNum3];
+
+     var randomNum4 = Math.floor(Math.random() * more_japan_seriesImg.length);
+    document.getElementById("more_japan_series" + i.toString()).src = more_japan_seriesImg[randomNum4];
+
+    var randomNum5 = Math.floor(Math.random() * more_japan_animeImg.length);
+    document.getElementById("more_japan_anime" + i.toString()).src = more_japan_animeImg[randomNum5];
+
+}
 }
 
 var jojoImg = new Array(
@@ -46,28 +59,49 @@ var jojoImg = new Array(
   "assets/img/images main/jojo p6.jpg",
 );
 
+var animeImg = new Array(
 
+  "assets/img/images main/one piece.jpg",
+  "assets/img/images main/akame.jpg",
+  "assets/img/images main/yamada.jpg",
+  "assets/img/images main/chainsaw.jpg",
+  "assets/img/images main/tqq2.jpg",
+  "assets/img/images main/solo.jpg",
+  
+  );
 
+  var japan_series = new Array(
+    
+    "assets/img/images main/tg.png",
+    "assets/img/images main/ds.jpg",
+    "assets/img/images main/dsm.webp",
+    "assets/img/images main/jjk.jpg",
+    "assets/img/images main/je veux manger.jpg",
+    "assets/img/images main/jjkm.jpg",
+  )
 
-function videohover2 (videolocation, video,img){
-  var videolocation =document.querySelector(videolocation)
-  var video =document.querySelector(video)
-  var image = document.querySelector(img)
+  var more_japan_seriesImg = new Array(
 
-  videolocation.addEventListener('mylistRandom', function (){
-      video.play()
-      $(video).show()
-      $(image).hide()
-  })
-  videolocation.addEventListener('mylistRandom', function (){
-      video.pause()
-      $(video).hide()
-      $(image).show()
-  });
-}
+    "assets/img/images main/redo.png",
+    "assets/img/images main/hanako.jpg",
+    "assets/img/images main/ngnl.jpg",
+    "assets/img/images main/devilman.jpg",
+    "assets/img/images main/dorohedoro.jpg",
+    "assets/img/images main/ajin.png",
+    
+    );
+  
+    var more_japan_animeImg = new Array(
 
-videohover('.mainhover','.videogantz1')
-videohover2('.carouselhoverimg2','.videogantz2',".hoverimg2")
+      "assets/img/images main/haikyuu.jpg",
+      "assets/img/images main/parasyte.jpg",
+      "assets/img/images main/mp100.jpg",
+      "assets/img/images main/tye.webp",
+      "assets/img/images main/kaguya.jpg",
+      "assets/img/images main/blue spring.webp",
+      
+      );   
+
 
 
 
